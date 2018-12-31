cd9683bb40066de96edb3fa95756c6842b92d128
akjasim <43490556+akjasim@users.noreply.github.com>
2018-12-31 08:36:43 +0530

    Update settings.py
    Deleted secret key.

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '==qmt_9j44cyqx(#o$h*b7673h60f6^=nu+*m2tfq92csuapqq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
